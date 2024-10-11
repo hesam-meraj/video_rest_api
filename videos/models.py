9c341f8bb9ecce1be56ddc3def6008c1f50251c9
hesam-meraji <mh.meraji12345@gmail.com>
2024-10-11 12:15:56 +0330

    Video models added

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,54 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    # for handling subcategories i do this below technique 
+    parent = models.ForeignKey('self',
+                               verbose_name='parent',
+                               blank=True,
+                               null=False,
+                               on_delete=models.CASCADE)
+    # if we delete the parent categories the child will be deleted too 
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True, upload_to='categories')
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+
+    class Meta:
+         db_table = 'categories' # name of the table in database
+         verbose_name = 'Category' # name in admin 
+         verbose_name_plural = 'Categories'
+
+         
+
+
+class Video(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    avatar = models.ImageField(blank=True, upload_to='vidoes/')
+    is_enable = models.BooleanField(default=True)
+    categories = models.ManyToManyField(Category, verbose_name='categories', blank=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)    
+
+    class Meta:
+         db_table = 'vidoes' # name of the table in database
+         verbose_name = 'Video' # name in admin 
+         verbose_name_plural = 'Videos'
+
+
+# i have created this because we may have several videos that are related to each other
+# i assumed we have series like rick and morty which have the same avatar and title
+class File(models.Model): 
+    video = models.ForeignKey(Video, verbose_name='video', on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='files/%Y/%m/%d/')
+    is_enable = models.BooleanField(default=True)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)    
+    
+    class Meta:
+         db_table = 'files' # name of the table in database
+         verbose_name = 'File' # name in admin 
+         verbose_name_plural = 'Files'
